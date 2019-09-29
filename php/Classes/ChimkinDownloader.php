e15009cfc1cb2522670f039b76a7a7838f08ab8f
CodeCanna <fleabass4@gmail.com>
2019-09-29 17:39:18 -0600

    Data downloader is getting data, now to parse it.

--- php/Classes/ChimkinDownloader.php
+++ php/Classes/ChimkinDownloader.php
@@ -1,12 +1,13 @@
 <?php
-require_once(direname(__DIR__, 1) . "vendor/autoload.php");
+require_once(dirname(__DIR__, 1) . "/vendor/autoload.php");
+require_once(".something.php");
 
 class ChimkinDownloader {
     public function __construct($request) {
         $key = getApiKey();
 		$curl = curl_init();
 		curl_setopt_array($curl, array(
-			CURLOPT_URL => "https://ebird.org/ws2.0/data/obs/US-NM/recent",
+			CURLOPT_URL => $request,
 			CURLOPT_RETURNTRANSFER => true,
 			CURLOPT_ENCODING => "",
 			CURLOPT_MAXREDIRS => 10,
@@ -15,11 +16,15 @@ class ChimkinDownloader {
 			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
 			CURLOPT_CUSTOMREQUEST => "GET",
 			CURLOPT_HTTPHEADER => array(
-				"X-eBirdApiToken: $key"
+				"x-api-key: $key"
 			),
         ));
         
         $response = curl_exec($curl);
-		$jsonArray = json_decode($response, true);
+        $jsonArray = json_decode($response, true);
+        
+        var_dump($jsonArray);
     }
 }
+
+$thing = new ChimkinDownloader("https://api.thecatapi.com/v1/images/search?breed_ids=sphy");
