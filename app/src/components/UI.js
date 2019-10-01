fc21b1535f61fdd852259b394785b06525121f5c
CodeCanna <fleabass4@gmail.com>
2019-09-30 19:54:46 -0600

    Removed old data downloader.

--- app/src/components/UI.js
+++ app/src/components/UI.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import { Container, Button, Form } from 'react-bootstrap';
+import { Container, Button, Form, Row, Col } from 'react-bootstrap';
 
 export default class UI extends React.Component {
     render(props) {
         return (
             <>
-                <a href={this.props.url} download={this.props.id} className="btn btn-primary">Download</a>
+                <Row>
+                    <Col>
+                        <Button>Prev</Button>
+                    </Col>
+                    <Col>
+                        <a href={this.props.url} download={this.props.id} className="btn btn-primary">Download</a>
+                    </Col>
+                    <Col>
+                        <form>
+
+                        </form>
+                        <button action="submit" className="btn btn-primary">Next</button>
+                    </Col>
+                </Row>
             </>
         );
     }
